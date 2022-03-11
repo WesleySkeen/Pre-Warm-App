accc0a20ef4800a2cebbd9765c7682088821af37
Wesley Skeen <wesley.skeen@emailage.com>
2022-03-11 14:15:30 +0000

    Add : Added a hosted service

---
+++ WeatherForecast.Web.Api/HostedServices/CheckWarmUpHostedService.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WeatherForecast.Web.Api.HostedServices
+{
+    public class CheckWarmUpHostedService : IHostedService, IDisposable
+    {
+        private readonly ILogger<CheckWarmUpHostedService> _logger;
+        private Timer _timer = null!;
+
+        public CheckWarmUpHostedService(ILogger<CheckWarmUpHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Timed Hosted Service running.");
+
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void DoWork(object state)
+        {
+            _logger.LogInformation("Ticking");
+        }
+
+        public Task StopAsync(CancellationToken stoppingToken)
+        {
+            
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
--- WeatherForecast.Web.Api/Startup.cs
+++ WeatherForecast.Web.Api/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using WeatherForecast.Web.Api.HostedServices;
 
 namespace WeatherForecast.Web.Api
 {
@@ -31,6 +25,7 @@ namespace WeatherForecast.Web.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherForecast.Web.Api", Version = "v1" });
             });
+            services.AddHostedService<CheckWarmUpHostedService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
